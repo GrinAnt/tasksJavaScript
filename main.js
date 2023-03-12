bcd5d7d345fa6684cbaaad49e3c624c16c809cce
GrinAnt <kanes@inbox.ru>
2023-03-12 20:50:13 +0300

    slider done

---
+++ main.js
@@ -0,0 +1,14 @@
+let arrow = document.querySelectorAll('.btn');
+let slide = document.querySelector('.slider__list');
+let firstImg = slide.querySelectorAll('.slider__item')[0];
+let firstImgWidth = firstImg.clientWidth + 2;
+
+arrow.forEach(function (icon) {
+	icon.addEventListener('click', function() {
+		if(icon.id == 'left')  {
+			slide.scrollLeft -= firstImgWidth;
+		} else {
+			slide.scrollLeft += firstImgWidth;
+		}
+	})
+})
