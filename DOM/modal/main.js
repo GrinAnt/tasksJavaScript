d6bfe906cb81f0a4d7afe94db52d91c645c78da8
GrinAnt <kanes@inbox.ru>
2023-03-13 00:06:20 +0300

    modal done

---
+++ DOM/modal/main.js
@@ -0,0 +1,60 @@
+const elemModal = document.createElement('div');
+const elemClose = document.createElement('span');
+const elemText = document.createElement('p');
+const container = document.querySelector('.container');
+let btn = document.querySelector('.btn');
+
+
+function addElem(block, elem, cls, content) {
+	block.appendChild(elem);
+	elem.classList.add(cls);
+	elem.textContent = content;
+}
+
+addElem(container, elemModal, 'modal');
+addElem(elemModal, elemClose, 'modal__close', 'X');
+addElem(elemModal, elemText, 'modal__text', 'Hello, my modal windows!');
+
+
+
+// Открывает модальное окно по нажатию на кнопку
+btn.addEventListener('click', function() {
+	elemModal.style.display = 'block';
+})
+
+// Закрывает модальное окно по нажатию на крестик
+elemClose.addEventListener('click', function() {
+	elemModal.style.display = 'none';
+})
+
+// Закрывает модальное окно по клику в любой части окна
+window.addEventListener('click', function(e) {
+	if (!e.target.closest('.modal') && !e.target.closest('.btn')) {
+		elemModal.style.display = 'none';
+	} 
+})
+
+
+
+// let modal = document.querySelector('.modal');
+// let close = document.querySelector('.modal__close');
+
+// Закрывает модальное окно по клику в любой части окна (не срабатывает)
+//  document.addEventListener('click', function(e) {
+// 		if (e.target == modal) {
+// 			console.log('закрыл');
+// 			modal.style.display = 'none';
+// 		}
+// 	})
+
+
+
+// Закрывает модальное окно по нажатию ESC (не срабатывает)
+// document.addEventListener('keydown', function(e) {
+// 	if (e.codeKey == 27) {
+// 		console.log('esc');
+// 		modal.style.display = 'none';
+// 	}
+// })
+
+
