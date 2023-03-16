bd59c8ccf76620ad1f814c339d1108da321691b2
GrinAnt <kanes@inbox.ru>
2023-03-16 08:00:42 +0300

    Переделал в функцию

--- Common/task_1.js
+++ Common/task_1.js
@@ -1,16 +1,31 @@
 // #1 Фильтрация строки
-const string = 'Привет мир! А вот и я';
-const listVowels = ["а","е","ё","и","о","у","ы","э","ю","я","А","Е","Ё","И","О","У","Ы","Э","Ю","Я"]
-let vowels = string.split('');
+const string = "Привет мир! А вот и я!";
+const listVowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я", "А", "Е", "Ё", "И", "О", "У", "Ы", "Э", "Ю", "Я"];
 let result = [];
+let resultEnd = "";
 
-for (let i = 0; i < string.length; i++) {
-	for (let j = 0; j < listVowels.length; j++) {
-		if (string[i] === listVowels[j]) {
-			result.push(string[i])
-		}
-	}
-} 
+function stringVowels(s, v) {
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < v.length; j++) {
+      if (s[i] === v[j]) {
+        result.push(s[i]);
+      }
+      resultEnd = result.join("");
+    }
+  }
+  return resultEnd;
+}
 
-let stringVowels = result.join('');
-console.log(stringVowels);
+console.log(stringVowels(string, listVowels));
+
+// ВТОРОЙ ВАРИАНТ
+// for (let i = 0; i < string.length; i++) {
+//   for (let j = 0; j < listVowels.length; j++) {
+//     if (string[i] === listVowels[j]) {
+//       result.push(string[i]);
+//     }
+//   }
+// }
+
+// let stringVowels = result.join("");
+// console.log(stringVowels);
